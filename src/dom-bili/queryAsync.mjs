2c727fe61b5544077c841bdaaf6e0fda09ef05c6
HHsomeHand <54211593+HHsomeHand@users.noreply.github.com>
2024-12-07 15:09:23 +0800

    queryAsync enhance

--- src/dom-bili/queryAsync.mjs
+++ src/dom-bili/queryAsync.mjs
@@ -1,5 +1,11 @@
-export function queryAsync(selector, timeout = 1000) {
-    return new Promise((resolve, reject) => {
+const errEl = document.createElement('div');
+errEl.classList.add('no-found');
+errEl.remove = () => {};
+
+// timeout 为-1时 无限查询并等待
+export function queryAsync(selector, timeout = 1000, onError = null) {
+    // 这里不用reject是担心脚本报错后, 终止运行
+    return new Promise((resolve) => {
         const startTime = Date.now();
 
         function check() {
@@ -8,8 +14,12 @@ export function queryAsync(selector, timeout = 1000) {
                 resolve(ele); // 找到元素，结束 Promise
                 return;
             }
-            if (Date.now() - startTime > timeout) {
-                reject(`$Q_Async: Timeout: Cannot find element for selector "${selector}"`);
+            if (timeout !== -1 && Date.now() - startTime > timeout) {
+                if (onError !== null) {
+                    onError();
+                }
+                resolve(errEl);
+                console.warn(`$Q_Async: Timeout: Cannot find element for selector "${selector}"`);
                 return;
             }
 
