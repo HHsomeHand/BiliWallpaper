939900e7e55e38ea293bba2023bb001c4193a9c3
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-03-02 20:08:14 +0800

    [bug fix] 修复了脚本失效的 bug

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue';
 import './assets/css/bilibili.css'
 import { useMainStore } from './store/main.mjs';
 import { storeToRefs } from 'pinia';
+import {queryAsync} from "@/dom-bili/queryAsync.mjs";
 
 const pinia = createPinia();
 
@@ -19,20 +20,25 @@ GM_registerMenuCommand('设置 (点我切换背景图片)', () => {
   isShowDialog.value = true;
 });
 
-app.mount(
-  (() => {
-    const app = document.createElement('div');
-    // let parent = document.body;
-    let parent = document.querySelector('.bili-header');
+(async () => {
+    let biliHeaderEl = await queryAsync('.bili-header');
 
-     // 将新节点插入为第一个子节点
-    if (parent.firstChild) {
-      parent.insertBefore(app, parent.firstChild);
-    } else {
-      parent.appendChild(app); // 如果父节点没有子节点，直接添加
-    }
+    app.mount(
+        (() => {
+            const app = document.createElement('div');
+            // let parent = document.body;
+            let parent = biliHeaderEl;
 
-    return app;
-  })(),
-);
+            // 将新节点插入为第一个子节点
+            if (parent.firstChild) {
+                parent.insertBefore(app, parent.firstChild);
+            } else {
+                parent.appendChild(app); // 如果父节点没有子节点，直接添加
+            }
+
+            return app;
+        })(),
+    );
+})();
+
 
